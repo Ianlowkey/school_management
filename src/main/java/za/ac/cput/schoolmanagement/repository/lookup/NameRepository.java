330e91eb71796cc9b9fa1a8a0bbdfcef274d33a9
Vuyisa <218192215@mycput.ac.za>
2022-06-18 01:17:02 +0200

    Repository Commit

--- src/main/java/za/ac/cput/schoolmanagement/repository/lookup/NameRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/lookup/NameRepository.java
@@ -8,9 +8,11 @@ package za.ac.cput.schoolmanagement.repository.lookup;
  */
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.schoolmanagement.domain.lookup.Name;
 import java.util.List;
 
+@Repository
 public interface NameRepository extends JpaRepository <Name, String> {
     List<Name> findAll();
 }
